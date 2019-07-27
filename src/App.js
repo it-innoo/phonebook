a2db916b0371bee54c2baaa4e37956eefb011ad2
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-07-27 12:10:39 +0300

    Step 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const createdPerson = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(createdPerson))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) =>
+    setNewName(event.target.value)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          Nimi:
+          <input
+            onChange={handleNameChange} value={newName}
+          />
+        </div>
+        <div>
+          <button type="submit">Lisää</button>
+        </div>
+      </form>
+
+      <h2>Numerot</h2>
+      {persons.map(p =>
+        <div key={p.name}>
+          {p.name}
+        </div>
+      )}
     </div>
-  );
+  )
+
 }
 
 export default App;
