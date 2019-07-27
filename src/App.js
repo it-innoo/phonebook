68d79a687a91776a019f0fc90b9e2cb454e61299
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-07-27 17:52:13 +0300

    Step 6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Filter = ({ handleChange, value }) => {
   return (
@@ -43,15 +45,24 @@ const PersonForm = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+
+  const personsToShow = filter.length === 0
+    ? persons
+    : persons.filter(p =>
+      p.name.toLowerCase().includes(filter.toLowerCase()))
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -83,10 +94,6 @@ const App = () => {
 
   const handleFilterChange = (event) =>
     setFilter(event.target.value)
-
-  const personsToShow = filter.length === 0
-    ? persons
-    : persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
 
   return (
     <div>
